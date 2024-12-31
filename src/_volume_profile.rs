6ceb43a56cb15b4da688b16d9999f81bfa8f10b8
Hindy <yuenshingyan@gmail.com>
2024-12-31 14:44:48 +0800

    use rayon

--- src/_volume_profile.rs
+++ src/_volume_profile.rs
@@ -1,14 +1,15 @@
 use pyo3::prelude::*;
 use std::collections::HashMap;
 use std::error::Error;
+use rayon::prelude::*;
 
 #[pyfunction]
 pub fn compute_volume_profile(
     close: Vec<f64>,
-    volume: Vec<i64>,
+    volume: Vec<f64>,
     bins: usize,
     window: usize,
-) -> (Vec<Option<HashMap<String, Vec<f64>>>>, Vec<Option<f64>>) {
+) -> (Vec<Option<f64>>, Vec<Option<HashMap<String, Vec<f64>>>>) {
     if close.len() != volume.len() {
         panic!(
             "Lenth of argument `close` ({}) must share the same length with argument `volume` ({})",
@@ -25,30 +26,28 @@ pub fn compute_volume_profile(
         );
     }
 
+    let mut close_min = 0.0;
     let mut close_max = 0.0;
     for c in &close {
+        close_min = f64::min(close_min, *c);
         close_max = f64::max(close_max, *c);
     }
+    let bin_width = (close_max - close_min) / bins as f64;
 
-    let offset = window - 1;
-    let mut point_of_control = vec![None; offset];
-    let mut volume_profile = vec![None; offset];
-    for i in 0..close.len() {
-        if i + window > close.len() {
-            break;
-        } else {
+     (0..close.len()).into_par_iter().map(|i| {
+        if i >= window {
             let histogram = compute_histogram(
-                &close[i..i + window],
-                &volume[i..i + window],
+                &close[i - window..i],
+                &volume[i - window..i],
                 bins,
-                close_max / bins as f64,
+                bin_width,
             )
             .expect("failed to compute histogram");
-            point_of_control.push(Some(compute_point_of_control(&histogram)));
-            volume_profile.push(Some(histogram));
+            (Some(compute_point_of_control(&histogram)), Some(histogram))
+        } else {
+            (None, None)
         }
-    }
-    (volume_profile, point_of_control)
+    }).collect::<(Vec<Option<f64>>, Vec<Option<HashMap<String, Vec<f64>>>>)>()
 }
 
 fn compute_point_of_control(histogram: &HashMap<String, Vec<f64>>) -> f64 {
@@ -62,15 +61,17 @@ fn compute_point_of_control(histogram: &HashMap<String, Vec<f64>>) -> f64 {
 
 fn compute_histogram(
     close_slice: &[f64],
-    volume_slice: &[i64],
+    volume_slice: &[f64],
     bins: usize,
     bin_width: f64,
 ) -> Result<HashMap<String, Vec<f64>>, Box<dyn Error>> {
+    if close_slice.len() != volume_slice.len() {
+        panic!("Argument 'close_slice' ({}) must share the same length with 'volume_slice' ({})", close_slice.len(), volume_slice.len());
+    }
+
     let mut volume_profile = HashMap::<String, Vec<f64>>::new();
     for i in 0..close_slice.len() {
-        let frequency = volume_slice[i] as f64;
-
-        for n in 0..bins {
+        for n in 1..=bins {
             let lower = n as f64 * bin_width;
             let upper = (n + 1) as f64 * bin_width;
             let middle = (lower + upper) / 2.0;
@@ -84,11 +85,11 @@ fn compute_histogram(
                     let existing_freq = existing_bin[existing_bin.len() - 1];
                     volume_profile.insert(
                         interval.clone(),
-                        vec![lower, upper, middle, existing_freq + frequency],
+                        vec![lower, upper, middle, existing_freq + volume_slice[i]],
                     );
                     break;
                 } else {
-                    volume_profile.insert(interval.clone(), vec![lower, upper, middle, frequency]);
+                    volume_profile.insert(interval.clone(), vec![lower, upper, middle, volume_slice[i]]);
                     break;
                 }
             }
